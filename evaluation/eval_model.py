2b500baf26b0a8cecb885b24551012600b270e5c
wl <1258481281@qq.com>
2019-03-18 21:53:26 +0800

    添加rouge,meteor评价功能

--- evaluation/eval_model.py
+++ evaluation/eval_model.py
@@ -10,7 +10,6 @@ def main(args):
     dec_dir = join(args.decode_dir, 'rouge_dec_dir')
 
     ref_dir = join(args.decode_dir, 'rouge_ref')
-    print(ref_dir)
     assert exists(ref_dir)
 
     if args.rouge:
