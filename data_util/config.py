d723a69dfdce50db6218866e752a1f9f51e4b241
wl <1258481281@qq.com>
2019-03-19 16:28:51 +0800

    调整超参数

--- data_util/config.py
+++ data_util/config.py
@@ -9,17 +9,17 @@ vocab_path = os.path.join(root_dir, "make_data/finished_files/vocab")
 log_root = os.path.join(root_dir, "log")
 
 # Hyperparameters
-hidden_dim= 256
-emb_dim= 128
-batch_size= 8
-max_enc_steps=400
-max_dec_steps=100
-beam_size=4
-min_dec_steps=35
-vocab_size=50000
+hidden_dim = 256
+emb_dim = 128
+batch_size = 8
+max_enc_steps = 100
+max_dec_steps = 20
+beam_size = 4
+min_dec_steps = 6
+vocab_size = 50000
 
 lr=0.15
-adagrad_init_acc=0.1
+adagrad_init_acc = 0.1
 rand_unif_init_mag=0.02
 trunc_norm_init_std=1e-4
 max_grad_norm=2.0
